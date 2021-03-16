d5913b475df9464e0f39d66ad9e944f0da80ef65
Mariia Hunko <masha.gunko96@gmail.com>
2021-03-16 17:07:05 +0200

    Пофиксила шестое

--- js/task-6.js
+++ js/task-6.js
@@ -1,12 +1,13 @@
 const inputValidationRef = document.getElementById('validation-input');
-
+console.log(inputValidationRef);
 inputValidationRef.addEventListener('blur', handleValidationInput);
 inputValidationRef.addEventListener('input', onDefaultStyle);
 
     function handleValidationInput(event) {
-        const length = +this.dataset.length;
-
+        const length = +inputValidationRef.dataset.length;
         const currentTargetValueLength = event.currentTarget.value.length;
+        console.log(length);
+        console.log(currentTargetValueLength);
 
         if (currentTargetValueLength === length) {
 
@@ -17,4 +18,8 @@ inputValidationRef.addEventListener('input', onDefaultStyle);
             inputValidationRef.classList.add('invalid');
             inputValidationRef.classList.remove('valid');
         }
+}
+
+function onDefaultStyle() {
+    inputValidationRef.classList.remove('valid', 'invalid');
 }
